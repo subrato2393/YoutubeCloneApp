b7722feba593a0b12146110dac2ee2bcebe13c97
Subrato Ghosh <subrato2393@gmail.com>
2021-07-19 12:21:19 +0600

    create initial project from visual studio

---
+++ YoutubeClone/YoutubeClone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YoutubeClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
