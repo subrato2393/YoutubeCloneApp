2aedfb25aff83857d4647ea7737c9b656ed2cf3f
Subrato Ghosh <subrato2393@gmail.com>
2021-07-24 01:44:44 +0600

    Call model from AccountController RegisterAction

--- YoutubeClone/YoutubeClone/Models/RegisterModel.cs
+++ YoutubeClone/YoutubeClone/Models/RegisterModel.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using Autofac;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeClone.Membership.Constant;
+using YoutubeClone.Membership.Entities;
 
 namespace YoutubeClone.Models
 {
@@ -23,5 +30,63 @@ namespace YoutubeClone.Models
         public string ConfirmPassword { get; set; }
         public string ReturnUrl { get; set; }
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+       
+        public RegisterModel(SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public RegisterModel()
+        {
+            _signInManager = Startup.AutofacContainer.Resolve<SignInManager<ApplicationUser>>();
+            _userManager = Startup.AutofacContainer.Resolve<UserManager<ApplicationUser>>();
+        }
+
+        public async Task GetExternalLoginProviderAsync()
+        {
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IdentityResult> CreateUserAsync()
+        {
+            var user = GenerateUser();
+          
+            return  await _userManager.CreateAsync(user, Password);
+        }
+
+        private ApplicationUser GenerateUser()
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = Email,
+                Email = Email,
+            };
+
+            return user;
+        }
+
+        public async Task AddRoleToUser()
+        {
+            var user = await GetUser();
+
+            await _userManager.AddToRoleAsync(user, MemberRole.UserRole);
+        }
+
+        public async Task SetBrowserCookie()
+        {
+          var user = await GetUser();
+
+          await _signInManager.SignInAsync(user, isPersistent: false);                  
+        }
+
+        public async Task<ApplicationUser> GetUser()
+        {
+           return await _userManager.FindByEmailAsync(this.Email);
+        }
     }
 }
